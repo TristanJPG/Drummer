79c0948447200267bd5f609248b726667580f051
TristanJPG <54478295+TristanJPG@users.noreply.github.com>
2020-10-19 18:14:44 -0400

    initial commit.
    All of the drummer files.

---
+++ index.js
@@ -0,0 +1,128 @@
+
+
+// another way to  get all buttons  is a for loop: 
+var thisRef = this;
+var numberOfDrumsButtons = document.querySelectorAll(".drum").length
+
+for  (var i = 0;  i < numberOfDrumsButtons;  i++ ) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML =  this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+        }
+    );
+}
+document.addEventListener("keydown", function() {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+
+
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+
+            var kick = new Audio("sounds/kick-bass.mp3");
+
+            kick.play();
+            break;
+    
+        case "a": 
+            var snare = new Audio("sounds/snare.mp3");
+
+            snare.play();
+            break;
+        
+        case "s":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+        case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+        case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+                break;
+        case "l": 
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+             break;
+        default:  
+            console.log("default")
+
+    }
+}
+
+
+
+
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("." + currentKey)
+   activeButton.classList.add("pressed"); 
+   setTimeout(function() {
+       activeButton.classList.remove("pressed");
+   }, 100); 
+
+}
+
+
+
+
+
+
+
+//  document.querySelector(".set").addEventListener("click",  function() {
+//     var buttonInnerHTML =  thisRef.innerHTML;
+//     console.log(buttonInnerHTML)
+//     switch (buttonInnerHTML) {
+//         case "w":
+
+//             var kick = new Audio("sounds/kick.mp3");
+
+//              kick.play();
+//             break;
+    
+//         case "a": 
+//             var snare = new Audio("sounds/snare.mp3");
+
+//             snare.play();
+//             break;
+        
+//         case "s":
+//             var crash = new Audio("sounds/crash.mp3");
+//             crash.play();
+//             break;
+//         case "d":
+//             var tom1 = new Audio("sounds/tom-1.mp3")
+//             tom1.play();
+//             break;
+//         case "m":
+//                 var tom2 = new Audio("sounds/tom-2.mp3");
+//                 tom2.play();
+//                 break;
+//         case "k":
+//                 var tom3 = new Audio("sounds/tom-3.mp3")
+//                 tom3.play();
+//                 break;
+//         case "l": 
+//             var tom4 = new Audio("sounds/tom-4.mp3")
+//             tom4.play();
+//              break;
+//         default:  
+//             console.log(buttonInnerHTML);
+        
+//     }
+//  });
+
+
